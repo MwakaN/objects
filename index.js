082dd84ab196d7a228de8a55b85a2e0167ca2d81
MwakaN <mwachimba@yahoo.com>
2023-02-27 23:35:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Question 1
 function InstagramPost (handleOfAuthor, content,imageLinkPostedByAuthor, numberOfLikes,numberOfViews){
     this.handleOfAuthor=handleOfAuthor;
     this.content=content;
@@ -11,6 +12,8 @@ console.log(Post1);
 let Post2 = new InstagramPost('National Geographic', 'African wildlife', 'http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcSpU5h6hjPVJAV7GUxnCE1h_XAl09dlDS-pQSUTtSFKAUo_h_muWVy0NRKbmwx9-wqVRS9GALE0CbwEm9Y', 10000, 6000)
 console.log(Post2);
 
+
+//Question 2
 /* InstagramPost {
   handleOfAuthor: 'Mwaka',
   content: 'Life is beautiful',
@@ -26,19 +29,20 @@ InstagramPost {
   numberOfViews: 6000
 
   */
-  function createPerson(name, age, location) {
+
+//Question 3
+//a)
+function createPerson(name, age, location) {
     return {
       name: name,
       age: age,
       location: location,
     }
 } 
+let Person = createPerson('Musa Dawodu', 19, 'Lagos State')
+console.log(Person); //{ name: 'Musa Dawodu', age: 19, location: 'Lagos State' } 
 
-let Person = createPerson('Musa Dawodu', 19, 'Lagos State');
-console.log(Person);
-
-//{ name: 'Musa Dawodu', age: 19, location: 'Lagos State' } 
-
+//b)
 function createJambScores(eng, govt, lit, crk) {
     return {
       eng: eng,
@@ -47,8 +51,31 @@ function createJambScores(eng, govt, lit, crk) {
       crk: crk,
     }
 } 
-
 let Score = createJambScores(70, 85, 82, 94 );
-console.log(Score);
+console.log(Score); //{ eng: 70, govt: 85, lit: 82, crk: 94 }
 
-//{ eng: 70, govt: 85, lit: 82, crk: 94 }
+
+//Question 4
+//Different ways to clone an object:
+//Using the Spread operator
+const newPerson = {
+    name: "Marg Begg", 
+    age: 60}
+let clonedPerson = {...newPerson}
+console.log(clonedPerson) //{ name: 'Marg Begg', age: 60 }
+
+//Using the Object.assign function
+const personDetails = {
+    name: "John Smith", 
+    age: 45}
+let clonedDetails = Object.assign({}, personDetails)
+console.log(clonedDetails) //{ name: 'John Smith', age: 45 }
+
+const foodType = {
+    vegetables: 'spinach', 
+    fruits: 'oranges' };
+let cloneFoodType = JSON.parse(JSON.stringify(foodType));
+console.log(cloneFoodType); //{ vegetables: 'spinach', fruits: 'oranges' }
+
+
+//Question 5
